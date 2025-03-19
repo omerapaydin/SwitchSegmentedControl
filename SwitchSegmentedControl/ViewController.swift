a88f55881678c682a8003d57edc76ad49e0c8cd3
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-19 20:09:28 +0300

    First

--- SwitchSegmentedControl/ViewController.swift
+++ SwitchSegmentedControl/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swtchUI: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func switvhtKonum(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func tikla(_ sender: Any) {
+        print("Switch Durum : \(swtchUI.isOn)")
+    }
+    
 }
 
