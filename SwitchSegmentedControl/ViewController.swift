59bd49ccb0fa97dc8729c11b7519afbff89725f0
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-19 22:08:09 +0300

    segmentIn

--- SwitchSegmentedControl/ViewController.swift
+++ SwitchSegmentedControl/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var swtchUI: UISwitch!
     
+    @IBOutlet weak var segmentIn: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,11 +19,30 @@ class ViewController: UIViewController {
 
     @IBAction func switvhtKonum(_ sender: Any) {
         
+        if swtchUI.isOn{
+            print("Switch Durum : AÇIK")
+        }else{
+            print("Switch Durum : KAPALI")
+        }
+        
+        
+        
     }
     
+    @IBAction func segment(_ sender: Any) {
+        
+        if segmentIn.selectedSegmentIndex == 0{
+            print("Segment Açık")
+        }else {
+                print("Segment Kapalı")
+        }
+        
+        
+    }
     
     @IBAction func tikla(_ sender: Any) {
         print("Switch Durum : \(swtchUI.isOn)")
+        print("Segment index: \(self.segmentIn.selectedSegmentIndex)")
     }
     
 }
