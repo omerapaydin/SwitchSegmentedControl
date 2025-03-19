8166292adb1156f0efae91ba3c9d686e6fe0b19a
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-19 17:09:29 +0300

    Initial Commit

---
+++ SwitchSegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwitchSegmentedControl
+//
+//  Created by Ömer on 19.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwitchSegmentedControlTests/SwitchSegmentedControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchSegmentedControlTests.swift
+//  SwitchSegmentedControlTests
+//
+//  Created by Ömer on 19.03.2025.
+//
+
+import Testing
+@testable import SwitchSegmentedControl
+
+struct SwitchSegmentedControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
